941946d61f4fe006204e0ca3612c7d7187dd6957
khmiq <mumtozabonukhamidullayeva7@gmail.com>
2025-03-07 10:09:39 +0500

    fixed

--- src/components/TableNumberDisplay.jsx
+++ src/components/TableNumberDisplay.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import bcg2 from "../assets/bcg2.jpg"
+import bcg2 from "../assets/bcg2.jpg";
+
 const TableNumberDisplay = () => {
-  const { tableNumber } = useParams(); 
+  const { tableNumber } = useParams();
 
   return (
-    <div 
-      className="flex items-center justify-center bg-cover bg-center text-amber-800" 
-      style={{ backgroundImage: `url(${bcg2})` }} 
+    <div
+      className="w-full min-h-screen flex flex-col items-center justify-center bg-no-repeat bg-contain bg-center my-2 rounded-lg"
+      style={{ backgroundImage: `url(${bcg2})` }}
     >
       {tableNumber && (
-        <div className="text-center">
-          <h2 className="text-5xl font-great font-bold">Ваш стол</h2>
-          <p className="text-7xl font-bold mt-2">{tableNumber}</p>
+        <div className="text-center text-amber-800 p-6">
+          <h2 className="text-6xl font-great ">Ваш стол</h2>
+          <p className="text-9xl font-great mt-4">{tableNumber}</p>
         </div>
       )}
     </div>
